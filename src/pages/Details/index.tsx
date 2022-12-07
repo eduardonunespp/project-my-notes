9af3f0857064f1ae6c4daaf32360448d7970be33
Carlos Eduardo <carlospn200406@gmail.com>
2022-12-07 15:10:00 -0300

    .

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,20 @@
+import { Container } from './style'
+//import {Fragment} from 'react'
+export function Details(){
+
+
+  return(
+    
+    <Container>
+
+      <h1>Hello World!, meu primeiro código em react</h1> 
+
+      <span>Teste</span>
+ 
+    </Container>
+  
+  )
+
+
+}
+
