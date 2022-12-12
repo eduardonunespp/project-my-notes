4e6621c97faba7232a46ad7b58abae3d78dc4900
Carlos Eduardo <carlospn200406@gmail.com>
2022-12-12 10:25:06 -0300

    1

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
